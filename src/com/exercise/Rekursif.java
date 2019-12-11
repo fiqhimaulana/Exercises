9cbb5369f760ff3e39a2a5f44c5dfe10d5d2804d
fiqhimaulana <fiqhimaulana1994@gmail.com>
2019-12-11 15:08:47 +0800

    Initial Commit

--- src/com/exercise/Rekursif.java
+++ src/com/exercise/Rekursif.java
@@ -4,17 +4,17 @@ public class Rekursif {
 
     public static void main(String[] Args) {
 
-        long hasil = faktorial(1);
+       // long hasil = faktorial(1);
         //System.out.println("5! = " +hasil);
         System.out.println("7! = " +faktorial(7));
     }
 
     private static long faktorial(long n) {
 
-        if (n == 0) {
+        if (n <= 0) {
             return (1);
         }
         else
-            return (n * faktorial(n-1));
+            return (n * faktorial(n-1)); // fungsi balik
     }
 }
